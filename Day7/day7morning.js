5ba4c8c60ca898af77f65bc2dd8b01286e212a35
Nikki Levy <Nikki@Nikkis-MacBook-Pro.local>
2016-06-07 13:56:45 +0200

    Day7 Morning Submission

---
+++ Day7/day7morning.js
@@ -0,0 +1,19 @@
+// window.onload = function() {
+	// console.log the values of First Name and Last Name 
+	function getFormValues() {
+
+		var formElement=document.getElementById("first").value;
+		var formElements=document.getElementById("last").value;
+		console.log (formElement);
+		console.log (formElements);
+
+
+	}
+
+	// Change the color of the div with id "color-div"
+	function changeColor() {
+
+		document.getElementById("color-div").style.color="red";
+	}
+
+// }
